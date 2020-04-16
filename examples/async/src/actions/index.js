23563e03f3690c21e87e60e98e159eab01733553
alan phan <41087230+AlanPhanChan@users.noreply.github.com>
2020-04-16 16:09:34 +0700

    i have analyzed redux boilerplate. need nore patient

--- examples/async/src/actions/index.js
+++ examples/async/src/actions/index.js
@@ -3,33 +3,33 @@ export const RECEIVE_POSTS = 'RECEIVE_POSTS'
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
 export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
 
-export const selectSubreddit = subreddit => ({
+export const selectSubreddit = (subreddit) => ({
   type: SELECT_SUBREDDIT,
-  subreddit
+  subreddit,
 })
 
-export const invalidateSubreddit = subreddit => ({
+export const invalidateSubreddit = (subreddit) => ({
   type: INVALIDATE_SUBREDDIT,
-  subreddit
+  subreddit,
 })
 
-export const requestPosts = subreddit => ({
+export const requestPosts = (subreddit) => ({
   type: REQUEST_POSTS,
-  subreddit
+  subreddit,
 })
 
 export const receivePosts = (subreddit, json) => ({
   type: RECEIVE_POSTS,
   subreddit,
-  posts: json.data.children.map(child => child.data),
-  receivedAt: Date.now()
+  posts: json.data.children.map((child) => child.data),
+  receivedAt: Date.now(),
 })
 
-const fetchPosts = subreddit => dispatch => {
+const fetchPosts = (subreddit) => (dispatch) => {
   dispatch(requestPosts(subreddit))
   return fetch(`https://www.reddit.com/r/${subreddit}.json`)
-    .then(response => response.json())
-    .then(json => dispatch(receivePosts(subreddit, json)))
+    .then((response) => response.json())
+    .then((json) => dispatch(receivePosts(subreddit, json)))
 }
 
 const shouldFetchPosts = (state, subreddit) => {
@@ -43,7 +43,7 @@ const shouldFetchPosts = (state, subreddit) => {
   return posts.didInvalidate
 }
 
-export const fetchPostsIfNeeded = subreddit => (dispatch, getState) => {
+export const fetchPostsIfNeeded = (subreddit) => (dispatch, getState) => {
   if (shouldFetchPosts(getState(), subreddit)) {
     return dispatch(fetchPosts(subreddit))
   }
